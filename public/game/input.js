a7c0f1a93860462173e0d9f3a5d901e5ead52d7e
Tiger Oakes <tigeroakes@gmail.com>
2016-12-03 14:22:45 -0800

    Added shake animation and listener

--- public/game/input.js
+++ public/game/input.js
@@ -1,5 +1,7 @@
 const { Manager, Swipe, DIRECTION_ALL } = Hammer; // import Hammer;
 
+const FUSE_LENGTH = 415;
+
 class Game {
 	constructor(
 		inputId = 'wordInput',
@@ -11,32 +13,29 @@ class Game {
 		this.valid = Promise.reject();
 
 		const input = document.getElementById(inputId);
-		input.addEventListener('change', (e) => {
-			var value = e.target.value;
-			this.valid = fetch(`../checkword/${value}`, { method: HEAD })
-				.then(response => response.ok);
-		});
-
 		const text = document.getElementById(textId);
 		const fuse = document.getElementById(fuseId);
-
-		const fuseLength = fuse.getTotalLength();
-		fuse.style.transition = 'none';
-		fuse.style.strokeDasharray = `${fuseLength}`;
 		fuse.style.strokeDashoffset = 0;
 
 		const bomb = document.getElementById(bombId);
 		const mc = new Manager(bomb);
 		mc.add(new Swipe({ direction: DIRECTION_ALL }));
 
-		mc.on('swipe', (e) => this.valid.then(
-			() => {
+		Object.assign(this, { input, text, fuse, bomb, mc });
+
+		mc.on('swipe', (e) => this.valid
+			.then(() => {
 				// TODO submit
-			},
-			() => {
-				// TODO error
 			})
+			.catch(() => this.bomb.style.animationName = 'shake')
 		);
+		input.addEventListener('change', (e) => {
+			var value = e.target.value;
+			this.valid = fetch(`../checkword/${value}`, { method: HEAD })
+				.then(response => response.ok);
+		});
+		bomb.addEventListener('animationEnd',
+			() => this.bomb.style.animationName = '');
 	}
 
 	setText(newLetters) {
