02f68e6504eec1e115dd61393ef6224b54fe743b
Tiger Oakes <tigeroakes@gmail.com>
2016-12-03 11:42:53 -0800

    Attached socket.io to Hapi server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const { Server } = require('hapi');
 const Inert = require('inert');
+const Socket = require('socket.io');
 
 const { route: validateWord } = require('./validWord.js');
 const { publicFiles } = require('./public.js');
 
 const server = new Server();
 server.connection({ port: 8000 });
+
+const io = Socket(server.listener);
+
 server.register(Inert).then(() => server.route(publicFiles));
 server.route(validateWord);
 
-module.exports = server;
+module.exports = { server, io };
