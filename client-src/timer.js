cd5c52bb900a5db21399dfde46714b6690d4c129
Tiger Oakes <tigeroakes@gmail.com>
2016-12-03 11:39:13 -0800

    Added eventlistener for timer end

--- client-src/timer.js
+++ client-src/timer.js
@@ -3,10 +3,14 @@
 /**
  * @param {number} time (usings seconds as units)
  * @param {string} [fuseID] ID of the fuse element
+ * @param {boolean} nolistener if true, doesn't listen for animation end.
+ * @returns {Promise<void>} resolves when animation ends.
+ * If nolistener is set, resolves immediately.
  */
-function animateFuse(time, fuseID = 'fuse') {
+function animateFuse(time, fuseID = 'fuse', nolistener = false) {
 	const path = document.getElementById(fuseID);
 	const length = path.getTotalLength();
+	path.style.transition = 'none';
 
 	path.style.strokeDasharray = `${length}`;
 	path.style.strokeDashoffset = 0;
@@ -14,4 +18,9 @@ function animateFuse(time, fuseID = 'fuse') {
 	path.getBoundingClientRect();
 	path.style.transition = `stroke-dashoffset ${time}s linear`;
 	path.style.strokeDashoffset = length;
+
+	if (nolistener) return Promise.resolve();
+	return new Promise(resolve =>
+		path.addEventListener('transitionend', resolve, { once: true })
+	);
 }
