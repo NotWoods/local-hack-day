28202e2719fa7bde9da9aabedcdf6a3a0c8ec4c0
Tiger Oakes <tigeroakes@gmail.com>
2016-12-03 15:59:52 -0800

    Ported Generator

---
+++ src/generateString.js
@@ -0,0 +1,19 @@
+const vowels = ['A', 'E', 'I', 'O', 'U'];
+const consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
+
+function rand(max) {
+	return Math.floor(Math.random() * max);
+}
+
+/**
+ * @returns {string} Random string 2-3 letters long
+ */
+module.exports = function generateString() {
+	const n = rand(1);
+	const v = rand(4);
+	const c = rand(20);
+
+	let str = `${consonants[c]}${vowels[v]}`;
+	if (n > .5) str += consonants[rand(20)];
+	return str;
+}
