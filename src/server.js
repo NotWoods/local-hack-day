91c60cfcc98820d8d8d5d5b820177b874a0c7b52
Tiger Oakes <tigeroakes@gmail.com>
2016-12-03 19:50:06 -0800

    Added NODE_ENV switch

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const { route: validateWord } = require('./validWord.js');
 const { publicFiles } = require('./public.js');
 
 const server = new Server();
-server.connection({ port: 7000 });
+server.connection({
+	port: process.env.NODE_ENV === 'production'
+		? 80
+		: 7000
+});
 
 server.register(Inert).then(() => server.route(publicFiles));
 server.route(validateWord);
